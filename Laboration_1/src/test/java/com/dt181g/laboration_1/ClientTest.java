a98caf26ae0577f6ce096520879303ac6540ef60
jola2311 <jola2311@student.miun.se>
2024-09-25 13:15:09 +0300

    testfile added

---
+++ Laboration_1/src/test/java/com/dt181g/laboration_1/ClientTest.java
@@ -0,0 +1,15 @@
+package com.dt181g.laboration_1;
+
+import org.junit.jupiter.api.Test;
+
+public class ClientTest {
+    @Test
+    void testNotifyWorksDone() {
+
+    }
+
+    @Test
+    void testRun() {
+
+    }
+}
