196f18fea540578d173f5bc93f1529f11cdbc0f6
jola2311 <jola2311@student.miun.se>
2024-10-09 15:10:59 +0300

    setup of lab3

---
+++ Laboration_3/src/main/java/com/dt181g/laboration_3/Support/DebugLogger.java
@@ -0,0 +1,77 @@
+package com.dt181g.laboration_3.Support;
+
+import java.util.logging.ConsoleHandler;
+import java.util.logging.Level;
+import java.util.logging.LogRecord;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
+
+public enum DebugLogger {
+    /**
+    * Singleton instance of the DebugLogger enum.
+    */
+    INSTANCE;
+    private final Logger LOGGER = Logger.getLogger("Logger");
+
+    /*================================
+        ↓ CONSTRUCTOR
+    ================================*/
+
+    /**
+     * Initializes the enum {@code DebugLogger} instance by attaching a
+     * {@code ConsoleHandler} set to INFO level with a simplified
+     * {@code SimpleFormatter} that logs messages directly.     *
+     */
+    private DebugLogger() {
+            // Initializes the console handler
+            final ConsoleHandler consoleHandler = new ConsoleHandler();
+
+            // Configures the handler
+            consoleHandler.setFormatter(new SimpleFormatter() {
+                @Override
+                public synchronized String format(final LogRecord logRecord) {
+                    return String.format("%s%n", logRecord.getMessage());
+                }
+            });
+
+            // Configures the logger
+            LOGGER.addHandler(consoleHandler);
+            LOGGER.setLevel(Level.INFO);
+            LOGGER.setUseParentHandlers(false); // Disable the default console handler to avoid duplicate logs
+    }
+
+    /*================================
+        ↓ Logger Methods
+    ================================*/
+
+    /**
+     * Logs the provided message with INFO level.
+     *
+     * @param message the message to be logged
+     */
+    private void performLog(String message) {
+        LOGGER.info(message);
+    }
+
+    // ===== Statistical logs =====
+
+    /**
+     * Logs information with a yellow color formatting.
+     *
+     * @param message the message to be logged
+     */
+    public void logInfo(String message) {
+        performLog(String.format("%s%s%s", AppConfig.ANSI_YELLOW, message, AppConfig.ANSI_RESET));
+
+    }
+
+
+    /**
+     * Logs warning information with a red color formatting.
+     *
+     * @param message the message to be logged
+     */
+    public void logWarning(String message) {
+        performLog(String.format("%s%s%s", AppConfig.ANSI_RED, message, AppConfig.ANSI_RESET));
+    }
+}
